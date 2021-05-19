09ab04f8d90b634a7e719a232360f6af7d33d0a5
Özgür Uysal <ozguruysal@gmail.com>
2021-05-19 11:14:35 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# hubspot-tailwind
+# Hubspot Tailwind CSS Starter
+
+Starter project for building HubSpot themes with Tailwind CSS and Webpack.
+
+### Install
+
+Run `yarn` or `npm install` to install dependencies
+
+### Scripts
+- `start` : Builds project with webpack, uploads to your `defaultPortal` specified in `hubspot.config.yml` and watches for changes via [`@hubspot/webpack-cms-plugins/HubSpotAutoUploadPlugin`](https://www.npmjs.com/package/@hubspot/webpack-cms-plugins).
+- `build` : Clears `/dist` contents and builds project into `/dist`.
+- `deploy` : Clears `/dist` contents, builds project into `/dist`, and uploads to via [`@hubspot/cms-cli`](https://www.npmjs.com/package/@hubspot/cms-cli).
+- `upload`: Uploads the `/dist` contents to your `defaultPortal` specified in `hubspot.config.yml`.
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HubSpotAutoUploadPlugin = require("@hubspot/webpack-cms-plugins/HubSpotAutoUploadPlugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = ({ portal, autoupload }) => ({
+  mode: "production",
+
+  entry: "./src/js/main.js",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
+  },
+
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, "src"),
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new HubSpotAutoUploadPlugin({
+      portal,
+      autoupload,
+      src: "dist",
+      dest: "hubspot-tailwind",
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/images", to: "images" },
+        { from: "src/templates", to: "templates" },
+        { from: "src/modules", to: "modules" },
+      ],
+    }),
+  ],
+});
